e017338033d34c5741da635a091cac3854a024b8
Akash Shrestha <aakashtha49@gmail.com>
2024-09-05 10:48:33 -0400

    Project added

---
+++ Huli Pizza/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Huli Pizza
+//
+//  Created by Akash Shrestha on 2024-09-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Huli Pizza/Huli_PizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Huli_PizzaApp.swift
+//  Huli Pizza
+//
+//  Created by Akash Shrestha on 2024-09-05.
+//
+
+import SwiftUI
+
+@main
+struct Huli_PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView() 
+        }
+    }
+}
